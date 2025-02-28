9dc2d8a6fba017578fef506e4c0ff7d77c9c71a8
User <Anyutka.g@rambler.ru>
2025-02-28 22:46:44 +0500

    5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,3 +5,21 @@ public class Main {
 
     }
 }
+
+
+/*      Задание 2
+Создать массив из 5 товаров.
+    Пример:
+    // вначале объявляем массив объектов
+    Product[] productArray = new Product[5];
+    // потом для каждой ячейки массива задаем объект
+    productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025",
+            "Samsung Corp.”, "Korea" , 5599, true);
+    productsArray[1] = new Product(...);
+    ...
+    productsArray[4] = new Product(...);
+*/
+/*      Задание 3
+Создать класс Park с внутренним классом, с помощью объектов которого можно хранить
+информацию об аттракционах, времени их работы и стоимости.
+*/
