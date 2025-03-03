f14e0e4faa798a434aaca9d7c7be9b01bce71b5d
User <Anyutka.g@rambler.ru>
2025-03-03 22:57:16 +0500

    24

--- src/main/java/org/example/Lesson_3/Main.java
+++ src/main/java/org/example/Lesson_3/Main.java
@@ -157,8 +157,8 @@ public class Main {
     */
     public static void arrayNumberOneHundred() {
         System.out.println("\n\n№ 11");
-        int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
+        int[] arr = new int[100]; // объявляю массив длиной 100
+        for (int i = 0; i < arr.length; i++) { // заполняю
             arr[i] = i + 1;
         }
         System.out.print(Arrays.toString(arr));
