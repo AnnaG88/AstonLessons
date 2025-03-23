52f8e8e8607bfe0cec9a7a5a1deb465ac8735494
User <Anyutka.g@rambler.ru>
2025-03-23 23:53:13 +0500

    49

---
+++ src/test/java/ArithmeticOperationTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArithmeticOperationTest {
+
+
+    @Test
+    @DisplayName("название теста")
+
+    public void someTest() {
+        System.out.println(" тест запустился ");
+
+
+    }
+
+}
