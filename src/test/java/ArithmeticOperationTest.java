be6a89f368b7b00eec0a1ef203661a6a5ad5e28c
User <Anyutka.g@rambler.ru>
2025-03-23 23:26:29 +0500

    49

---
+++ src/test/java/ArithmeticOperationTest.java
@@ -0,0 +1,13 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ArithmeticOperationTest {
+
+    @Test(description = "описание")
+    public void someTest(){
+        System.out.println(" тест ");
+
+        Assert.assertEquals(1,3);
+    }
+
+}
