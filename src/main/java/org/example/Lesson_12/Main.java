12925465ad265cec00777407dd60313c3a0b0451
User <Anyutka.g@rambler.ru>
2025-03-25 20:56:25 +0500

    50

--- src/main/java/org/example/Lesson_12/Main.java
+++ src/main/java/org/example/Lesson_12/Main.java
@@ -16,7 +16,7 @@ package org.example.Lesson_12;
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbers = { -1, 0, 1, 2}; // массив чисел для которых будем...
+        int[] numbers = { -1, 0, 1, 2, 3}; // массив чисел для которых будем...
         Factorial factorialCalculator = new Factorial(); // вычислять факториал
         for (int number : numbers) {
             String result = factorialCalculator.factorial(number);
---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,28 @@
+import org.example.Lesson_12.Triangle;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TriangleTest {
+
+
+    @Test
+    @DisplayName("тест для программы вычисления площади треугольника")
+
+    public void someTest() {
+        System.out.println(" тест запустился ");
+    }
+
+    @Test
+    public void triangleTest() {
+        Triangle triangle1 = new Triangle(3, 4, 5);
+        assertEquals(6.0, triangle1.area(), 0.0001, "S треугольника 3, 4, 5 = 6.0");
+
+        Triangle triangle2 = new Triangle(5, 12, 13);
+        assertEquals(30.0, triangle2.area(), 0.0001, "S треугольника 5, 12, 13 = 30.0");
+
+        Triangle triangle3 = new Triangle(7, 8, 9);
+        assertEquals(26.8328, triangle3.area(), 0.0001, "S треугольника 7, 8, 9 = 26.8328");
+    }
+
+}
